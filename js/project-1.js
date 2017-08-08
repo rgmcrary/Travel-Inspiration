e0bb88cb9f1f0b42f82811720b8e3a3d4880c681
Drew Mercer <japanexpress@gmail.com>
2017-08-08 19:43:55 -0400

    Adding project-specific js and including firebase

---
+++ js/project-1.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD1wYOxE3hqoAV9F3FBNXaQGXLWtDG3MMU",
+  authDomain: "project-1-175917.firebaseapp.com",
+  databaseURL: "https://project-1-175917.firebaseio.com",
+  projectId: "project-1-175917",
+  storageBucket: "project-1-175917.appspot.com",
+  messagingSenderId: "784499710582"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
