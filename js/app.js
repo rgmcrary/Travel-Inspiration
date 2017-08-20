fcf1b1660d6c661a646e0fe3a786eb2bcdedbb31
Rich McRary <rgmcrary@gmail.com>
2017-08-20 15:01:40 -0400

    updated reg ex input allowances

--- js/app.js
+++ js/app.js
@@ -15,10 +15,71 @@ var database = firebase.database();
 
 $(document).ready(function() {
   // Populate random quote into footer (content courtesy of http://theplanetd.com/the-ultimate-travel-quotes-as-chosen-by-you/)
-  var quotes = new Array('“Travel is  fatal to prejudice, bigotry, and narrow mindedness, and many of our people need it sorely on these accounts.” ~ Mark Twain','"The real voyage of discovery consists not in seeking new landscapes, but in having new eyes.” ~ Marcel Proust','“If you think adventure is dangerous, try routine, it is lethal.”','“Great things never came from comfort zones.”','“If you are always trying to be normal, you will never know how amazing you can be.”','“Stop worrying about the potholes in the road and enjoy the journey.”','“Fear is only temporary. Regrets last forever.”','“Life begins at the end of your comfort zone.”','“Travel does not become adventure until you leave yourself behind.”','“Climb the mountain so you can see the world, not so the world can see you."','“Man cannot discover new oceans unless he has the courage to lose sight of the shore.” ~ Andre Gide','“It is not the destination where you end up, but the mishaps and memories you create along the way.”','“I would rather own a little and see the world, than own the world and see a little of it.”','“It is better to see something once than to hear about it a thousand times.”','“The real voyage of discovery consists not in seeking new landscapes, but having new eyes.”','“I travel not to go anywhere, but to go. I travel for travel\'s sake.”','“Live with no excuses and travel with no regrets.”','“We travel not to escape life but for life not to escape us.”','“People don\'t take trips, trips take people.”','“Travel makes one modest, you see what a tiny place you occupy in the world.”','“Like all great travellers, I have seen more than I remember and remember more than I have seen.”','“Don\'t listen to what they say, go see.” ~ Chinese Proverb','“Whenever you find yourself on the side of majority, it\'s time to pause and reflect.”','“Those who follow the crowd usually get lost in it.”','“The world is changed by your example, not your opinion.”','“To travel is to discover that everyone is wrong about other countries.”','“You must be the change you wish to see in the world.”','“Don\'t quit your day dream.”','“There\'s a sunrise and sunset every single day, and they\'re absolutely free. Don\'t miss so many of them.”','“Life isn\'t about finding yourself. Life is about creating yourself.”','“If your ship doesn\'t come in, swim out to it.”','“Because when you stop and look around, this life is pretty amazing.”','“Without new experiences something inside us sleeps. The sleeper must awaken.”','“Doing what you like is freedom, liking what you do is happiness.”','“You only live once, but if you do it right, once is enough.” ~ Mae West','“This heart of mine was made to travel this world”','“I don\'t know where I\'m going, but I\'m on my way."','"The impulse to travel is one of the hopeful symptoms of life.”','“Let\'s find some beautiful place to get lost.”','“I\'m in love with cities I\'ve never been to and people I\'ve never met.”','“It\'s a big world out there, it would be a shame not to experience it.”','“You can shake the sand from your shoes, but not from your soul.”','“I want to make memories all over the world.”','“Someday I\'m going to be free and I\'m going to travel the world.”','“We wander for distraction, but we travel for fulfillment.”','“It doesn\'t matter where you\'re going, it\'s who you have beside you.”','“Traveling is not something you\'re good at. It\'s something you do, like breathing.” ~ Gayle Foreman','“All we have to decide is what to do with the time that is given us.”','“Let your memory be your travel bag.”','“It feels good to be lost in the right direction.”','“Traveling allows you to become so many different versions of yourself.”','“Travel…the best way to be lost and found at the same time.” ~ Brenna Smith','“Travel has a way of stretching the mind.”','“Oh, the places you\'ll go!" ~ Dr. Seuss','“Wandering re-establishes the original harmony which once existed between man and the universe.”','“All journeys have secret destinations of which the traveler is unaware.”','“A journey is best measured in friends rather than miles.”','“The journey is my home.”','“Just go. Go see all the beauty in the world.”','“Where to next?”');
-  randNo = quotes[Math.floor( Math.random() * quotes.length )];
-  $('.inspirationalQuote').text( randNo );
-  
+  var quotes = new Array(
+    "“Travel is  fatal to prejudice, bigotry, and narrow mindedness, and many of our people need it sorely on these accounts.” ~ Mark Twain",
+    '"The real voyage of discovery consists not in seeking new landscapes, but in having new eyes.” ~ Marcel Proust',
+    "“If you think adventure is dangerous, try routine, it is lethal.”",
+    "“Great things never came from comfort zones.”",
+    "“If you are always trying to be normal, you will never know how amazing you can be.”",
+    "“Stop worrying about the potholes in the road and enjoy the journey.”",
+    "“Fear is only temporary. Regrets last forever.”",
+    "“Life begins at the end of your comfort zone.”",
+    "“Travel does not become adventure until you leave yourself behind.”",
+    '“Climb the mountain so you can see the world, not so the world can see you."',
+    "“Man cannot discover new oceans unless he has the courage to lose sight of the shore.” ~ Andre Gide",
+    "“It is not the destination where you end up, but the mishaps and memories you create along the way.”",
+    "“I would rather own a little and see the world, than own the world and see a little of it.”",
+    "“It is better to see something once than to hear about it a thousand times.”",
+    "“The real voyage of discovery consists not in seeking new landscapes, but having new eyes.”",
+    "“I travel not to go anywhere, but to go. I travel for travel's sake.”",
+    "“Live with no excuses and travel with no regrets.”",
+    "“We travel not to escape life but for life not to escape us.”",
+    "“People don't take trips, trips take people.”",
+    "“Travel makes one modest, you see what a tiny place you occupy in the world.”",
+    "“Like all great travellers, I have seen more than I remember and remember more than I have seen.”",
+    "“Don't listen to what they say, go see.” ~ Chinese Proverb",
+    "“Whenever you find yourself on the side of majority, it's time to pause and reflect.”",
+    "“Those who follow the crowd usually get lost in it.”",
+    "“The world is changed by your example, not your opinion.”",
+    "“To travel is to discover that everyone is wrong about other countries.”",
+    "“You must be the change you wish to see in the world.”",
+    "“Don't quit your day dream.”",
+    "“There's a sunrise and sunset every single day, and they're absolutely free. Don't miss so many of them.”",
+    "“Life isn't about finding yourself. Life is about creating yourself.”",
+    "“If your ship doesn't come in, swim out to it.”",
+    "“Because when you stop and look around, this life is pretty amazing.”",
+    "“Without new experiences something inside us sleeps. The sleeper must awaken.”",
+    "“Doing what you like is freedom, liking what you do is happiness.”",
+    "“You only live once, but if you do it right, once is enough.” ~ Mae West",
+    "“This heart of mine was made to travel this world”",
+    "“I don't know where I'm going, but I'm on my way.\"",
+    '"The impulse to travel is one of the hopeful symptoms of life.”',
+    "“Let's find some beautiful place to get lost.”",
+    "“I'm in love with cities I've never been to and people I've never met.”",
+    "“It's a big world out there, it would be a shame not to experience it.”",
+    "“You can shake the sand from your shoes, but not from your soul.”",
+    "“I want to make memories all over the world.”",
+    "“Someday I'm going to be free and I'm going to travel the world.”",
+    "“We wander for distraction, but we travel for fulfillment.”",
+    "“It doesn't matter where you're going, it's who you have beside you.”",
+    "“Traveling is not something you're good at. It's something you do, like breathing.” ~ Gayle Foreman",
+    "“All we have to decide is what to do with the time that is given us.”",
+    "“Let your memory be your travel bag.”",
+    "“It feels good to be lost in the right direction.”",
+    "“Traveling allows you to become so many different versions of yourself.”",
+    "“Travel…the best way to be lost and found at the same time.” ~ Brenna Smith",
+    "“Travel has a way of stretching the mind.”",
+    "“Oh, the places you'll go!\" ~ Dr. Seuss",
+    "“Wandering re-establishes the original harmony which once existed between man and the universe.”",
+    "“All journeys have secret destinations of which the traveler is unaware.”",
+    "“A journey is best measured in friends rather than miles.”",
+    "“The journey is my home.”",
+    "“Just go. Go see all the beauty in the world.”",
+    "“Where to next?”"
+  );
+  randNo = quotes[Math.floor(Math.random() * quotes.length)];
+  $(".inspirationalQuote").text(randNo);
+
   // Slick responsive carousel
   $("#carousel").slick({
     dots: true,
@@ -62,7 +123,7 @@ function handleClick(location) {
 }
 
 function checkInput(input) {
-  return /^([a-zA-Z,. ]{2,})$/.test(input);
+  return /^([a-zA-Z,.- ]{2,})$/.test(input);
 }
 
 // Google Maps API
@@ -192,7 +253,15 @@ function getImages(location) {
     for (var i = 0; i < response.hits.length; i++) {
       //makeTags = JSON.stringify(response.hits[i].tags);
       //makeTagsParsed = JSON.parse(makeTags);
-      $("#output").append('<a  target="_blank" href="' + response.hits[i].pageURL + '">' + '<img class="z-depth-1 hoverable" src="' + response.hits[i].webformatURL + '" />' + '</a>');
+      $("#output").append(
+        '<a  target="_blank" href="' +
+          response.hits[i].pageURL +
+          '">' +
+          '<img class="z-depth-1 hoverable" src="' +
+          response.hits[i].webformatURL +
+          '" />' +
+          "</a>"
+      );
       //console.log(makeTags,response.hits[i].tags, makeTagsParsed);
       //$("#output").addClass("card-title", makeTags[0]);
       //$(".card").append('<span class="card-title">' + makeTags[0] + '</span>');
@@ -222,7 +291,9 @@ function getWeatherData(location) {
   $.getJSON(weatherURL).done(function(response) {
     console.log(response);
     $("#weather").html("");
-    $("#weather").append('<div class="card-panel teal col m1"> Date </br> Day </br> High (F) </br> Low (F) </br> Condition </br></div>');
+    $("#weather").append(
+      '<div class="card-panel teal col m1"> Date </br> Day </br> High (F) </br> Low (F) </br> Condition </br></div>'
+    );
     //query.results/channel.item.forcast
     for (
       var i = 0;
@@ -239,7 +310,19 @@ function getWeatherData(location) {
       html += '<div class="textStyle">' + result.text + "</div>";
       html += "</div>";
       //$("#weather").append(html);
-      $("#weather").append('<div class="card-panel teal col m2">' + result.date + ' </br>' + result.day +  ' </br>' + result.high + ' </br>' + result.low + ' </br>' + result.text + '</div>');
+      $("#weather").append(
+        '<div class="card-panel teal col m2">' +
+          result.date +
+          " </br>" +
+          result.day +
+          " </br>" +
+          result.high +
+          " </br>" +
+          result.low +
+          " </br>" +
+          result.text +
+          "</div>"
+      );
     }
   });
 }
@@ -254,7 +337,7 @@ function saveToFirebase(location) {
 
   $("#recentSearches").empty();
   database.ref().limitToLast(5).on("child_added", function(childSnapshot) {
-  recent = childSnapshot.val();
+    recent = childSnapshot.val();
     $("#recentSearches").prepend(recent.searched + "</br>");
   });
 }
